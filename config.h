a4f314fa2df954d593f2e705a8010f7751f46b26
Furnace5 <bencrosser316@gmail.com>
2021-04-29 12:18:46 +0000

    removed annoying title

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -16,7 +16,7 @@ static const char col_black[]       = "#000000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_cream, col_black, col_black },
-	[SchemeSel]  = { col_black, col_cream,  col_black  },
+	[SchemeSel]  = { col_black, col_cream,  col_cream  },
 };
 
 /* tagging */
